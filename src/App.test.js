e52547cc354e8e48cc9a787d7cf779fb13d69705
Acube <alex.adrien.auger@gmail.com>
2018-02-07 11:24:26 +0100

    Add test about the number of currencies

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+const assert = require('assert');
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('should have correct amount of currencies', () => {
+  const Currencies = require('./currencies.js');
+  const currenciesArray = Currencies.currenciesArray;
+  assert.strictEqual(currenciesArray.length, 32);
 });
